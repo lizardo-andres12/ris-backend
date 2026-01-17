75257325757f3387105938ae40f0a4a385f6da65
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-01-16 23:55:34 -0500

    [BE] wire embedding fetch logic in controller

--- internal/controller/search_controller.go
+++ internal/controller/search_controller.go
@@ -4,33 +4,37 @@ import (
 	"context"
 	"image"
 
+	"ris.com/internal/gateway"
 	"ris.com/internal/models"
 	"ris.com/internal/repository"
 )
 
-// SearchController manages all business logic and handles logging
+// SearchController manages all business logic and logging for similar image search functionality
 type SearchController interface {
+	// SearchSimilar requests a normalized embedding using its embeddingGate, queries similar images using that embedding,
+	// and returns a list of images similar to the embedding
 	SearchSimilar(ctx context.Context, image image.Image, limit, offset int) ([]*models.Image, error)
 }
 
 type searchController struct {
 	imageRepository repository.ImageRepository
+	embeddingGateway gateway.EmbeddingGateway
 }
 
 var _ SearchController = &searchController{}
 
-func NewSearchController(ir repository.ImageRepository) SearchController {
-	return &searchController{imageRepository: ir}
+// NewSearchController returns a new concrete implmenetation of the SearchController interface
+func NewSearchController(ir repository.ImageRepository, eg gateway.EmbeddingGateway) SearchController {
+	return &searchController{imageRepository: ir, embeddingGateway: eg}
 }
 
 func (sc *searchController) SearchSimilar(ctx context.Context, image image.Image, limit, offset int) ([]*models.Image, error) {
-//embedding, err := sc.embedderGateway.RequestEmbed(image)
-//if err != nil {
-//	// error log
-//	return nil, err
-//}
-	embedding := []float32{1, 2, 3}
-	images, err := sc.imageRepository.QuerySimilar(ctx, embedding, limit, offset)
+	embedding, err := sc.embeddingGateway.GetEmbedding(ctx, image)
+	if err != nil {
+		return nil, err
+	}
+
+	images, err := sc.imageRepository.QuerySimilar(ctx, embedding.Embedding, limit, offset)
 	if err != nil {
 		return nil, err
 	}
